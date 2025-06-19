7ad0a6672595564bf27cec9ed1785b4b939a6eba
Akash Bhaskar <i.am.akashbhaskar@gmail.com>
2025-06-19 23:17:51 +0530

    feat: google updates

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,7 +4,7 @@ export default registerAs('database', () => ({
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432', 10),
   username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgre',
   name: process.env.DB_DATABASE,
   synchronize: process.env.DB_SYNC === 'true',
   logging: process.env.DB_LOGGING === 'true',
