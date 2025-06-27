a851da9c85ca361166b5445b6da968bc4f0fd14b
Akash Bhaskar <i.am.akashbhaskar@gmail.com>
2025-06-27 12:22:41 +0530

    feat: betting db updates

--- src/stream/entities/stream.entity.ts
+++ src/stream/entities/stream.entity.ts
@@ -28,7 +28,7 @@ export class Stream extends BaseEntity {
   @Column({
     type: 'enum',
     enum: StreamStatus,
-    default: StreamStatus.SCHEDULED,
+    default: StreamStatus.LIVE,
   })
   status: StreamStatus;
 
