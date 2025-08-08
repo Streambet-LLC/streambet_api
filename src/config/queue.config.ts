cf8a7686eda59238523f844c03d02bbb8e313698
Vishnu Mohan <vishnumohan@revyrieglobal.com>
2025-08-08 09:34:33 +0530

    fix: Provide default values for Redis host and port in queue configuration

--- src/config/queue.config.ts
+++ src/config/queue.config.ts
@@ -2,8 +2,8 @@ import { registerAs } from '@nestjs/config';
 
 export const queueConfig = registerAs('queue', () => ({
   redis: {
-    host: process.env.REDIS_HOST,
-    port: parseInt(process.env.REDIS_PORT, 10),
+    host: process.env.REDIS_HOST || 'localhost',
+    port: parseInt(process.env.REDIS_PORT, 10) || 6379,
     password: process.env.REDIS_PASSWORD,
     username: process.env.REDIS_USERNAME,
   },
