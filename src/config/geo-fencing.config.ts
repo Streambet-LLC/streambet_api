f432577e99335f6a2cb6852f730f0abd29cd2018
reshmams@revyrieglobal.com <reshmams@revyrieglobal.com>
2025-08-13 18:29:33 +0530

    refactor: change geo-location to geo-fencingr Redis support

--- src/config/geo-fencing.config.ts
+++ src/config/geo-fencing.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('geo', () => ({
   abstractKey: process.env.ABSTRACT_API_KEY,
- blockedRegion: process.env.BLOCKED_REGION,
+  blockedRegion: process.env.BLOCKED_REGION,
   blockVPN: process.env.BLOCK_VPN,
   trustProxy: process.env.TRUST_PROXY,
 }));
