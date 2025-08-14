52024be748f44b71c853ca2fadb3f9da022e7a3f
reshmams@revyrieglobal.com <reshmams@revyrieglobal.com>
2025-08-14 11:00:17 +0530

    refactory(redis validation): validation  code for redis env variable - unpdated

--- src/config/redis.validation.ts
+++ src/config/redis.validation.ts
@@ -1,42 +1,36 @@
 import * as Joi from 'joi';
 
 export const envValidationSchema = Joi.object({
-  REDIS_HOST: Joi.string().hostname().required().label('REDIS_HOST').messages({
-    'string.hostname': '"REDIS_HOST" must be a valid hostname',
-    'any.required': '"REDIS_HOST" is required and cannot be empty',
-  }),
+  REDIS_HOST: Joi.alternatives()
+    .try(
+      Joi.string().hostname(),
+      Joi.string().ip({ version: ['ipv4', 'ipv6'] }),
+    )
+    .required()
+    .label('REDIS_HOST')
+    .messages({
+      'alternatives.match':
+        '"REDIS_HOST" must be a valid hostname or IP address',
+      'any.required': '"REDIS_HOST" is required and cannot be empty',
+    }),
 
   REDIS_PORT: Joi.number().port().required().label('REDIS_PORT').messages({
     'number.port': '"REDIS_PORT" must be a valid port number',
-    'any.required': '"REDIS_PORT" is required and cannot be empty',
   }),
 
-  REDIS_PASSWORD: Joi.string()
-    .allow('')
-    .required()
-    .label('REDIS_PASSWORD')
-    .messages({
-      'any.required':
-        '"REDIS_PASSWORD" is required (can be empty string if no password)',
-    }),
+  REDIS_PASSWORD: Joi.string().required().label('REDIS_PASSWORD').messages({
+    'any.required':
+      '"REDIS_PASSWORD" is required (can be empty string if no password)',
+  }),
 
-  REDIS_DB: Joi.number()
-    .min(0)
-    .required()
-    .default(0)
-    .label('REDIS_DB')
-    .messages({
-      'number.min': '"REDIS_DB" must be greater than or equal to 0',
-      'any.required': '"REDIS_DB" is required',
-    }),
-  REDIS_USERNAME: Joi.string()
-    .allow('')
-    .required()
-    .label('REDIS_USERNAME')
-    .messages({
-      'any.required':
-        '"REDIS_USERNAME" is required (can be empty string if no username)',
-    }),
+  REDIS_DB: Joi.number().min(0).required().label('REDIS_DB').messages({
+    'number.min': '"REDIS_DB" must be greater than or equal to 0',
+    'any.required': '"REDIS_DB" is required',
+  }),
+  REDIS_USERNAME: Joi.string().default('').label('REDIS_USERNAME').messages({
+    'any.required':
+      '"REDIS_USERNAME" is required (can be empty string if no username)',
+  }),
   REDIS_KEY_PREFIX: Joi.string()
     .allow('')
     .required()
