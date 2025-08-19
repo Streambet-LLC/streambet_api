2dc077e6d8e960761228e3c929dceeb0a0c89d4d
Vishnu Mohan <vishnumohan@revyrieglobal.com>
2025-08-19 19:51:47 +0530

    feature(betting): add StreamBetUpdated event emission to betting updates

--- src/betting/betting.service.ts
+++ src/betting/betting.service.ts
@@ -797,6 +797,7 @@ export class BettingService {
           bettingVariable.name,
           [], // No winners
         );
+        this.bettingGateway.emitStreamListEvent(StreamList.StreamBetUpdated)
         return;
       }
 
@@ -884,6 +885,9 @@ export class BettingService {
         bettingVariable.name,
         winners,
       );
+
+      this.bettingGateway.emitStreamListEvent(StreamList.StreamBetUpdated)
+
       for (const winner of winners) {
         await this.bettingGateway.emitBotMessageToWinner(
           winner.userId,
@@ -1658,6 +1662,8 @@ export class BettingService {
           );
         }
       }
+
+      this.bettingGateway.emitStreamListEvent(StreamList.StreamBetUpdated)
       return savedRound;
     } else {
       throw new BadRequestException(
@@ -1734,6 +1740,8 @@ export class BettingService {
           'canceled',
         );
       }
+
+      this.bettingGateway.emitStreamListEvent(StreamList.StreamBetUpdated)
 
       return { refundedBets };
     } catch (error) {
