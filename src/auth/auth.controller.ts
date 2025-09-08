2491b4b9909b674c9abac20d071b726e8e3c6f3c
reshmams@revyrieglobal.com <reshmams@revyrieglobal.com>
2025-09-08 14:28:38 +0530

    feat: create api for checking location is restricted or not

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -132,6 +132,25 @@ export class AuthController {
     };
   }
 
+  @ApiOperation({
+    summary: 'Check location is restricted or not',
+    description:
+      'This endpoint is used for developement. This will check the current user location is restricted or not.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Location is not restricted',
+  })
+  @UseGuards(GeoFencingGuard)
+  @Get('location-check')
+  async locationRestriction() {
+    return {
+      data: true,
+      message: 'Location is not restricted',
+      statusCode: HttpStatus.OK,
+    };
+  }
+
   @ApiOperation({ summary: 'Refresh access token using refresh token' })
   @ApiResponse({
     status: 200,
