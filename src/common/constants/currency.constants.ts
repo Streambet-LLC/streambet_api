d2067600bc215552da351506049d9fcbf24fd352
Aaron <aaron@streambet.tv>
2025-10-01 22:35:12 +0800

    updates sweep coins constants

--- src/common/constants/currency.constants.ts
+++ src/common/constants/currency.constants.ts
@@ -2,14 +2,14 @@
  * Fixed conversion rate between sweep coins and USD.
  * Example: 20 sweep coins = 1 USD.
  */
-export const SWEEP_COINS_PER_DOLLAR = 20;
+export const SWEEP_COINS_PER_DOLLAR = 1;
 
 /**
  * Minimum dollar amount allowed for withdrawal-related conversions.
  * This value is used to compute the minimum sweep coins required.
  */
 
-export const MIN_WITHDRAWABLE_SWEEP_COINS = 20;
+export const MIN_WITHDRAWABLE_SWEEP_COINS = 1;
 
 /**
  * The maximum allowed amount a user can place for a single bet.
