4baa31ac2b796e17e8439578a2e6f17c6402cb26
Bob Hampton <132041117+bobhampton@users.noreply.github.com>
2025-10-29 17:04:13 -0600

    fix: correct currency text pluralization for consistency

--- src/enums/currency.enum.ts
+++ src/enums/currency.enum.ts
@@ -5,6 +5,6 @@ export enum CurrencyType {
   GOLD_COINS = 'gold_coins',
 }
 export enum CurrencyTypeText {
-  SWEEP_COINS_TEXT = 'Stream Coin',
-  GOLD_COINS_TEXT = 'Gold Coin',
+  SWEEP_COINS_TEXT = 'Stream Coins',
+  GOLD_COINS_TEXT = 'Gold Coins',
 }
