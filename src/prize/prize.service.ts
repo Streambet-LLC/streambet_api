ede3a889d5d7ffd44fe64c92d1036a87c45090dd
aaron-streambet <aaron@streambet.tv>
2026-03-30 10:07:55 -0700

    updates seller email upon purchase

--- src/prize/prize.service.ts
+++ src/prize/prize.service.ts
@@ -1992,6 +1992,19 @@ export class PrizeService {
             )
           : order.totalPrice;
 
+      const frontendUrl = this.configService.get<string>(
+        'CLIENT_URL',
+        'http://localhost:3000',
+      );
+      const markShippedUrl = `${frontendUrl}/seller/shop/manage?tab=orders&orderId=${order.id}`;
+      const shipping: PrizeOrder['shippingAddress'] = order.shippingAddress || {
+        addressLine1: '',
+        city: '',
+        state: '',
+        zipCode: '',
+        country: '',
+      };
+
       await this.emailsService.sendEmailSMTP(
         {
           toAddress: [seller.email],
@@ -2007,6 +2020,14 @@ export class PrizeService {
               month: 'long',
               day: 'numeric',
             }),
+            buyerFullName: buyer.name || buyer.username,
+            shippingAddressLine1: shipping.addressLine1 || '',
+            shippingAddressLine2: shipping.addressLine2 || '',
+            shippingCity: shipping.city || '',
+            shippingState: shipping.state || '',
+            shippingZipCode: shipping.zipCode || '',
+            shippingCountry: shipping.country || '',
+            markShippedUrl,
           },
         },
         'seller_shop_purchase',
